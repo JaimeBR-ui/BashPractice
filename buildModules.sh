c5bfb55a920fa2f552619722649398c4e8396a7f
Jaime <52945689+JaimeBR-ui@users.noreply.github.com>
2020-07-26 15:04:53 -0400

    Create buildModules.sh

---
+++ buildModules.sh
@@ -0,0 +1,22 @@
+# Jaime Bohorquez
+
+# Script builds Node.js modules.
+
+# Mobile
+cd mobile
+npm install
+cd ..
+
+# Server
+cd server
+# API
+cd api
+npm install
+cd ..
+# Client
+cd client 
+npm install 
+cd ..
+cd ..
+
+exit
